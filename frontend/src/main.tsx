dd72ba52ab32f9211be87fe920c75862b20751db
niyaz04 <niyazhaque098@gmail.com>
2024-07-18 21:41:35 +0530

    removed devQuery

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import queryClient from "./config/queryClient.ts";
@@ -12,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
-        <ReactQueryDevtools position="bottom" initialIsOpen={false} />
+      
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
