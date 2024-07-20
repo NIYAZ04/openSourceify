ca2e360521cd58fe4a94b4e406c5a958d0710ef0
niyaz04 <niyazhaque098@gmail.com>
2024-07-20 15:49:50 +0530

    DeveloperList added

---
+++ DeveloperList.ts
@@ -0,0 +1,16 @@
+const developers = [
+  {
+    developerName: "YourName",
+    developerWorkHeading: "Short Title of Your Work/Change",
+    detailedWorkChanges: "Detailed description of what you did.",
+    developerEmail: "Your Email Address"
+  },
+  
+
+
+
+
+  // Add more entries as needed
+];
+
+export default developers;
