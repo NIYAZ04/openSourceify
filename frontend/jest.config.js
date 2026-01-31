dfa56ed6d8ce468f51b9c6a8757b84ee050be9dd
NIYAZ04 <niyazhaque098@gmail.com>
2026-02-01 00:02:24 +0530

    unit test jest.config.js added

---
+++ frontend/jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+};
